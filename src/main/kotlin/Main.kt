5ad84ff1e232b6e71c3098cd6ee1ced75f745367
Kakeru Takino <kakeru.takino@robinsons.co.jp>
2022-11-17 18:06:37 +0900

    add functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,31 +3,39 @@ const val MAX_EXPERIENCE: Int = 5000
 fun main(args: Array<String>) {
     val name = "Madrigal"
     var healthPoint = 89
-    val isBressed = true
+    val isBlessed = true
     val isImmortal = false
 
     /**
      * Aura
      */
-    val auraVisible = isBressed && healthPoint > 50 || isImmortal
+    val auraVisible = isBlessed && healthPoint > 50 || isImmortal
     val auraColor = if (auraVisible) "GREEN" else "NONE"
-    println(auraColor)
 
-    val healthStatus = if (healthPoint == 100) {
-        "is in excellent condition!"
-    } else if (healthPoint in 90..99) {
-        "has a few scratches."
-    } else if (healthPoint in 75..89) {
-        if (isBressed) {
-           "has some minor wounds but is healing quite quickly!"
-        }else{
+    val healthStatus = formatHealthStatus(healthPoint, isBlessed)
+    // Player status
+    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun formatHealthStatus(healthPoint: Int, isBlessed: Boolean): String {
+    val healthStatus = when (healthPoint) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds but is healing quite quickly!"
+        } else {
             "has some minor wounds."
         }
-    } else if (healthPoint in 15..74) {
-        "looks pretty hurt."
-    } else {
-        "is in awful condition!"
+
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
     }
-    // Player status
-    println("$name $healthStatus")
+    return healthStatus
+}
+
+private fun auraColor(isBlessed: Boolean,
+healthPoint: Int,
+isImmotal: Boolean): String {
+
 }
