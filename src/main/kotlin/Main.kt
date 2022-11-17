e707c3b35bd7677989d89dcfd0de6c2cd5730ba1
Kakeru Takino <kakeru.takino@robinsons.co.jp>
2022-11-17 15:48:50 +0900

    edited

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,33 @@
 const val MAX_EXPERIENCE: Int = 5000
 
 fun main(args: Array<String>) {
-    val playerName: String = "Estragon"
-    var experiencePoints: Int = 5
+    val name = "Madrigal"
+    var healthPoint = 89
+    val isBressed = true
+    val isImmortal = false
 
-    var hasSteed: String = ""
-    val pubName = "Unicorn's Horn"
-    val publican = "Verovero"
+    /**
+     * Aura
+     */
+    val auraVisible = isBressed && healthPoint > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    println(auraColor)
 
-    experiencePoints += 5
-    println(experiencePoints)
-    println(playerName)
+    val healthStatus = if (healthPoint == 100) {
+        "is in excellent condition!"
+    } else if (healthPoint in 90..99) {
+        "has a few scratches."
+    } else if (healthPoint in 75..89) {
+        if (isBressed) {
+           "has some minor wounds but is healing quite quickly!"
+        }else{
+            "has some minor wounds."
+        }
+    } else if (healthPoint in 15..74) {
+        "looks pretty hurt."
+    } else {
+        "is in awful condition!"
+    }
+    // Player status
+    println("$name $healthStatus")
 }
