cfddc09f360d9bee29199a044fcfcbd0e27666b1
Hyter_TK <kakeru.t0227@gmail.com>
2022-11-19 17:24:35 +0900

    add files

---
+++ src/main/kotlin/SwordJuggler.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var swordsJuggling: Int? = null
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+    if (isJugglingProficient) {
+        swordsJuggling = 2
+    }
+    proficiencyCheck(swordsJuggling)
+    println("You juggle $swordsJuggling swords!")
+}
+
+fun proficiencyCheck(swordsJuggling: Int?) {
+    swordsJuggling ?: throw UnskilledSwordJugglerException()
+}
+
+class UnskilledSwordJugglerException() : IllegalStateException("Player cannot juggle swords")
---
+++ src/main/kotlin/Tavern.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var beverage = readLine()?.let {
+        if(it.isNotBlank()) {
+            it.capitalize()
+        } else {
+            "Buttered Ale"
+        }
+    }
+    println(beverage)
+}
