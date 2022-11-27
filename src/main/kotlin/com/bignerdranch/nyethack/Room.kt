b01d25ddd707e4c5b9993529a8e5bbf574b51382
ktakino <kakeru.t0227@gmail.com>
2022-11-27 10:57:29 +0900

    edited

--- src/main/kotlin/com/bignerdranch/nyethack/Room.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Room.kt
@@ -7,10 +7,10 @@ open class Room(val name: String) {
     open fun load() = "Nothing much to see here..."
 }
 
-class TownSquare : Room("Town Square") {
+open class TownSquare : Room("Town Square") {
     override val dangerLevel: Int = super.dangerLevel - 3
     private var bellSound = "GWONG"
-    override fun load() = "The villagers rally and cheer as you enter!\n${ringBell()}"
+    final override fun load() = "The villagers rally and cheer as you enter!\n${ringBell()}"
 
     private fun ringBell() = "The bell tower announces your arrival. $bellSound"
 }
